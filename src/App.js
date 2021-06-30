3f4b7b1c242f8301efa86df634bfd5ad2613fcc1
Brian Firestone <bfirestone2339@gmail.com>
2021-06-29 22:13:15 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import './App.css';
-import React, {useState} from 'react';
+import "./App.css";
+import React, { useState } from "react";
 
 const App = () => {
-
-  const [{count, count2}, setCount] = useState({count: 10, count2: 20});
+  const [{ count1 }, setCount1] = useState({ count1: 0 });
+  const [{ count2 }, setCount2] = useState({ count2: 0 });
 
   return (
     <div>
-      <div>count1: {count}</div>
+      <div>count1: {count1}</div>
       <div>count2: {count2}</div>
-      <button onClick={() => 
-        setCount(currentState => ({count: currentState.count + 1, count2: currentState.count2 + 1}))}>Add</button>
+      <button
+        onClick={() =>
+          setCount1((currentState) => ({
+            ...currentState,
+            count1: currentState.count1 + 1,
+          }))
+        }
+      >
+        Count1
+      </button>
+      <button
+        onClick={() =>
+          setCount2((currentState) => ({
+            ...currentState,
+            count2: currentState.count2 + 1,
+          }))
+        }
+      >
+        Count2
+      </button>
     </div>
   );
-}
+};
 
 export default App;
